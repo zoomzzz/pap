e859b1b1613e191a969e6b389ca8e3846d0c70bf
jp209403@gmail.com <jp209403@gmail.com>
2022-07-01 16:19:15 +0100

    minor fixes

--- pages/colaps.js
+++ pages/colaps.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/page.module.css";
 import background from "../assets/background.jpg";
-import Navbar from "../components/nav/Navbar";
+import Navbar from "../components/Navbar";
 export default function Home() {
   const { videos, container, backgroundContainer } = styles;
   return (
